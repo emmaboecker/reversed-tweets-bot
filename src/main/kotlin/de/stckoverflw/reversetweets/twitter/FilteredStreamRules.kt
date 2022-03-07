2d4b4aef6285ba032714211e25ddbf63bd245e38
StckOverflw <stckoverflw@gmail.com>
2022-03-07 20:57:22 +0100

    Only send 1 request to register all stream rules

--- src/main/kotlin/de/stckoverflw/reversetweets/twitter/FilteredStreamRules.kt
+++ src/main/kotlin/de/stckoverflw/reversetweets/twitter/FilteredStreamRules.kt
@@ -7,10 +7,7 @@ import de.stckoverflw.reversetweets.config.Config
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import kotlinx.serialization.json.add
-import kotlinx.serialization.json.buildJsonObject
-import kotlinx.serialization.json.putJsonArray
-import kotlinx.serialization.json.putJsonObject
+import kotlinx.serialization.json.*
 import mu.KotlinLogging
 
 private val LOG = KotlinLogging.logger { }
@@ -18,7 +15,7 @@ private val LOG = KotlinLogging.logger { }
 suspend fun setUpFilteredStreamRules() {
     // Get existing TweetRules
     val rules = twitter.retrieveFilteredStreamRules()
-    LOG.debug("Found ${rules.size} rules!")
+    LOG.debug("Found ${rules?.size} rules!")
     // Delete existing TweetRules
     if (!rules.isNullOrEmpty()) {
         LOG.info("Deleting existing stream rules")
@@ -37,13 +34,22 @@ suspend fun setUpFilteredStreamRules() {
         }
     }
 
-    // Add Rules to reply to users
-    Config.TWITTER_REPLY_USERS.forEach {
-        twitter.addFilteredStreamRule("from:$it", "$it's tweets")
-        LOG.debug("Added '$it' to reply users")
+    client.post<HttpResponse>("https://api.twitter.com/2/tweets/search/stream/rules") {
+        header(HttpHeaders.Authorization, "Bearer ${Config.TWITTER_BEARER_TOKEN}")
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+        body = buildJsonObject {
+            putJsonArray("add") {
+                Config.TWITTER_REPLY_USERS.forEach {
+                    addJsonObject {
+                        put("value", "from:${twitter.getUserFromUserName(it).id}")
+                    }
+                }
+                addJsonObject {
+                    put("value", "to:${TwitterBot.self.name}")
+                }
+            }
+        }
     }
 
     LOG.info("Replying to: ${Config.TWITTER_REPLY_USERS.joinToString(", ")}")
-
-    twitter.addFilteredStreamRule("to:${TwitterBot.self.name}", "replies")
 }
