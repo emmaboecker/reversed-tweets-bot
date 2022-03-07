ca395ef018775cba61df67d822b9dab7b413053f
StckOverflw <stckoverflw@gmail.com>
2022-03-07 20:57:42 +0100

    make main file a coroutine scope

--- src/main/kotlin/de/stckoverflw/reversetweets/TwitterBot.kt
+++ src/main/kotlin/de/stckoverflw/reversetweets/TwitterBot.kt
@@ -12,20 +12,20 @@ import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import kotlinx.coroutines.*
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
 import mu.KotlinLogging
 import java.util.*
 import java.util.concurrent.ForkJoinPool
 import java.util.concurrent.Future
+import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.resumeWithException
 
 private val LOG = KotlinLogging.logger { }
 
-object TwitterBot {
+object TwitterBot : CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = Dispatchers.IO + SupervisorJob()
 
     lateinit var twitter: TwitterClient
     lateinit var client: HttpClient
@@ -33,67 +33,69 @@ object TwitterBot {
     lateinit var self: UserV2
 
     suspend operator fun invoke() {
-        // Log into Twitter
-        twitter = TwitterClient(
-            credentials {
-                apiKey = Config.TWITTER_API_KEY
-                apiSecretKey = Config.TWITTER_API_SECRET
-                accessToken = Config.TWITTER_ACCESS_TOKEN
-                accessTokenSecret = Config.TWITTER_ACCESS_SECRET
+        coroutineScope {
+            twitter = TwitterClient(
+                credentials {
+                    apiKey = Config.TWITTER_API_KEY
+                    apiSecretKey = Config.TWITTER_API_SECRET
+                    accessToken = Config.TWITTER_ACCESS_TOKEN
+                    accessTokenSecret = Config.TWITTER_ACCESS_SECRET
+                }
+            )
+            client = HttpClient(OkHttp) {
+                install(JsonFeature) {
+                    serializer = KotlinxSerializer()
+                }
             }
-        )
+            self = twitter.getUserFromUserName("me")
+            setUpFilteredStreamRules()
+            LOG.info("Starting Stream")
+            try {
+                twitter.startFilteredStream { tweet ->
+                    val author = twitter.getUserFromUserId(tweet.authorId)
+                    if (Config.TWITTER_DELETE_USERS.contains(author.name)) {
+                        if (tweet.tweetType == TweetType.REPLIED_TO) {
+                            if (tweet.text.lowercase(Locale.getDefault()).contains("/delete")) {
+                                if (self.id.equals(tweet.inReplyToUserId)) {
+                                    LOG.info("Delete Tweet:")
+                                    LOG.info(twitter.getTweet(tweet.inReplyToStatusId).text)
+                                    LOG.info("Because ${author.name} replied:")
+                                    LOG.info(tweet.text)
+                                    twitter.deleteTweet(tweet.inReplyToStatusId)
+                                }
+                                return@startFilteredStream
+                            }
+                        }
+                    }
+                    if (Config.TWITTER_REPLY_USERS.contains(author.name)) {
+                        if (tweet.tweetType != TweetType.RETWEETED) {
+                            LOG.info("${tweet.text} by ${author.name}")
+                            launch {
+                                var cleanText = tweet.text
+                                    .replace("^(@\\w+ )*".toRegex(), "")
+                                tweet.entities.urls?.forEach {
+                                    cleanText = cleanText.replace(it.url, "")
+                                }
 
-        // Initialize HttpClient
-        client = HttpClient(OkHttp) {
-            install(JsonFeature) {
-                serializer = KotlinxSerializer()
+                                val flippedImages = tweet.flipImages()
+
+                                val tweetBuilder = TweetParameters.builder()
+                                    .reply(TweetParameters.Reply.builder().inReplyToTweetId(tweet.id).build())
+                                    .text(cleanText.reversed())
+                                if (flippedImages != null) {
+                                    tweetBuilder.media(TweetParameters.Media.builder().mediaIds(flippedImages).build())
+                                }
+                                twitter.postTweet(tweetBuilder.build())
+                            }
+                        }
+                    }
+                }.await()
+            } catch (ex: Exception) {
+                LOG.error {
+                    "Catched an Error: \nMessage: ${ex.message} \nStacktrace: ${ex.stackTrace}"
+                }
             }
         }
-
-        self = twitter.getUserFromUserName("me")
-        setUpFilteredStreamRules()
-
-        LOG.info("Starting Stream")
-
-        twitter.startFilteredStream { tweet ->
-            val author = twitter.getUserFromUserId(tweet.authorId)
-            if (Config.TWITTER_DELETE_USERS.contains(author.name)) {
-                if (tweet.tweetType == TweetType.REPLIED_TO) {
-                    if (tweet.text.lowercase(Locale.getDefault()).contains("/delete")) {
-                        if (self.id.equals(tweet.inReplyToUserId)) {
-                            LOG.info("Delete Tweet:")
-                            LOG.info(twitter.getTweet(tweet.inReplyToStatusId).text)
-                            LOG.info("Because ${author.name} replied:")
-                            LOG.info(tweet.text)
-                            twitter.deleteTweet(tweet.inReplyToStatusId)
-                        }
-                        return@startFilteredStream
-                    }
-                }
-            }
-            if (Config.TWITTER_REPLY_USERS.contains(author.name)) {
-                if (tweet.tweetType != TweetType.RETWEETED) {
-                    LOG.info("${tweet.text} by ${author.name}")
-                    GlobalScope.launch {
-                        var cleanText = tweet.text
-                            .replace("^(@\\w+ )*".toRegex(), "")
-                        tweet.entities.urls?.forEach {
-                            cleanText = cleanText.replace(it.url, "")
-                        }
-
-                        val flippedImages = tweet.flipImages()
-
-                        val tweetBuilder = TweetParameters.builder()
-                            .reply(TweetParameters.Reply.builder().inReplyToTweetId(tweet.id).build())
-                            .text(cleanText.reversed())
-                        if (flippedImages != null) {
-                            tweetBuilder.media(TweetParameters.Media.builder().mediaIds(flippedImages).build())
-                        }
-                        twitter.postTweet(tweetBuilder.build())
-                    }
-                }
-            }
-        }.await()
     }
 }
 
